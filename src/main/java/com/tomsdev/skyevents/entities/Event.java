f9474f0046a3df879901f397d9d3e5e418383a5c
sta <sujithtom@gmail.com>
2018-10-25 23:44:27 +0100

    sample json

--- src/main/java/com/tomsdev/skyevents/entities/Event.java
+++ src/main/java/com/tomsdev/skyevents/entities/Event.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Data
@@ -20,5 +21,41 @@ public class Event {
     private Date eventEndDate;
     private String eventImage;
     private List<EventType> eventTypes;
-    private List<Location> visibleFromLocation;
+    private List<Location> visibleFromLocation; // TODO: introduce this later
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "eventName='" + eventName + '\'' +
+                ", eventDescription='" + eventDescription + '\'' +
+                ", eventShortDescription='" + eventShortDescription + '\'' +
+                ", eventStartDate=" + eventStartDate +
+                ", eventEndDate=" + eventEndDate +
+                ", eventTypes= { " + eventTypes.stream().map(EventType::toString).collect(Collectors.joining(", ")) + " }" +
+                // ", visibleFromLocation= { " + visibleFromLocation.stream().map(Location::toString).collect(Collectors.joining(", ")) + " }" +
+                '}';
+    }
+
+    public Event(String eventName, String eventDescription, String eventShortDescription, Date eventStartDate, Date eventEndDate,
+                 String eventImage, List<EventType> eventTypes, List<Location> visibleFromLocation) {
+        this.eventName = eventName;
+        this.eventDescription = eventDescription;
+        this.eventShortDescription = eventShortDescription;
+        this.eventStartDate = eventStartDate;
+        this.eventEndDate = eventEndDate;
+        this.eventImage = eventImage;
+        this.eventTypes = eventTypes;
+        this.visibleFromLocation = visibleFromLocation;
+    }
+
+    public Event(String eventName, String eventDescription, String eventShortDescription, Date eventStartDate, Date eventEndDate,
+                 String eventImage, List<EventType> eventTypes) {
+        this.eventName = eventName;
+        this.eventDescription = eventDescription;
+        this.eventShortDescription = eventShortDescription;
+        this.eventStartDate = eventStartDate;
+        this.eventEndDate = eventEndDate;
+        this.eventImage = eventImage;
+        this.eventTypes = eventTypes;
+    }
 }
