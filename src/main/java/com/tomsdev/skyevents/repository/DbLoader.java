8b21319c9c053a856fddb5fafc60f88d431c9dd6
sta <sujithtom@gmail.com>
2018-10-29 23:27:00 +0000

    Jackson + save to db

--- src/main/java/com/tomsdev/skyevents/repository/DbLoader.java
+++ src/main/java/com/tomsdev/skyevents/repository/DbLoader.java
@@ -1,8 +1,15 @@
 package com.tomsdev.skyevents.repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tomsdev.skyevents.entities.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 
 @Component
@@ -15,6 +22,17 @@ public class DbLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... strings) throws Exception {
+    public void run(String... strings) {
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<Event>> eventsType = new TypeReference<List<Event>>() {};
+        InputStream is = TypeReference.class.getResourceAsStream("/json/sampleData.json");
+        try {
+            List<Event> events = mapper.readValue(is, eventsType);
+            for (Event event:events) {
+                repository.save(event);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
